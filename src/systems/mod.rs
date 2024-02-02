17ea4ea8330522ee00d0014ef98956f091194672
G0nz0pera <155404796+g0nz0pera@users.noreply.github.com>
2024-02-02 16:13:42 -0300

    moving entities in a turn-based game

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -6,15 +6,23 @@ mod random_move;
 mod end_turn;
 
 use crate::prelude::*;
+use crate::systems::collisions::collisions_system;
 
 /*
-This function creates a Legion Schedule (an execution plan for our systems).
+These functions creates a Legion Schedule (an execution plan for our systems).
 t follows the builder pattern: Schedule::builder starts the system building process, and build() finishes it.
+This is very similar to our build_scheduler function, but now we’ve three schedulers.
+ Note that flush() is called when a system makes changes to the ECS dataset.
+ The systems in each phase are carefully divided by what makes sense:
+
+    While awaiting input, the screen still needs to display the map and entities. It also calls the player_input system.
+    When it’s the player’s turn, the game doesn’t accept input, but does check for collisions and renders everything. It finishes with end_turn.
+    The monsters’ turn is very similar to the player’s but adds random movement.
+
  */
-pub fn build_scheduler() -> Schedule {
+pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
-        .add_system(collisions::collisions_system())
         //  The flush() call is new. When a system executes commands, they don’t take effect immediately.
         //  There’s a hidden flush at the end of the systems that tell Legion (the ECS library) to apply changes immediately.
         //  Flushing after collision detection ensures that any deleted entities are really gone before they’re rendered.
@@ -24,7 +32,28 @@ pub fn build_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
-        .add_system(random_move::random_move_system())
+        .add_system(end_turn::end_turn_system())
+        .build()
+}
 
+pub fn build_player_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .add_system(end_turn::end_turn_system())
+        .build()
+}
+
+pub fn build_monster_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(random_move::random_move_system())
+        .flush()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
+        .add_system(end_turn::end_turn_system())
         .build()
 }
