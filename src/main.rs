ea13398cc1657899e0ec4eb3d755b36f30d8a66f
G0nz0pera <155404796+g0nz0pera@users.noreply.github.com>
2024-02-01 23:26:49 -0300

    init code that has the prelud config for the gamecode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod map;
+
+mod prelude {
+    //Publicly using the bracket_lib prelude re-exports it inside our prelude.
+    // Anything that uses our prelude also uses bracket_lib.
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use crate::prelude::*;
+//Rust commonly uses usize to index collections and arrays.
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT ) as usize;
+
 fn main() {
     println!("Hello, world!");
 }
