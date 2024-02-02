d1c00bbc0d50c5a78183910848ebc68a59a2139f
G0nz0pera <155404796+g0nz0pera@users.noreply.github.com>
2024-02-02 02:07:22 -0300

    adding more context data for the graphics

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH /2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT /2;
+
+    pub const DUNGEON_FONT: &str = "dungeonfont.png";
+
     pub use crate::map::*;
     pub use crate::player::*;
     pub use crate::map_builder::*;
@@ -54,6 +59,18 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
+        //This uses with_dimensions to specify the size of subsequent consoles we add.
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        // This uses with_tile_dimension that specifies the size of each character in our font file, in this case, 32x32.
+        .with_tile_dimensions(32, 32)
+        //This specifies the directory in which we placed the graphics file.
+        .with_resource_path("resources/")
+        //This is the name of the font file to load and the character dimensions. These are usually the same as tile dimensions but can be different in some advanced forms of rendering.
+        .with_font(DUNGEON_FONT, 32, 32)
+        //This adds a console using the dimensions already specified and the named tile graphics file.
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, DUNGEON_FONT)
+        // This adds a second console, with no background so that transparency shows through it.
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, DUNGEON_FONT)
         .build()?;
 
     main_loop(context, State::new())
