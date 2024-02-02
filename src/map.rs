f13cbb1e0e9c07b736458d4c46eb8d656ba80ff5
G0nz0pera <155404796+g0nz0pera@users.noreply.github.com>
2024-02-02 10:58:52 -0300

    removing the render function from the map.rs file

--- src/map.rs
+++ src/map.rs
@@ -34,43 +34,6 @@ impl Map {
     }
 
     /*
-    The function receives a borrowed Camera and uses the boundaries from the camera to render only the visible part of the map.
-    Notice that it now calls in_bounds to ensure that each tile exists.
-    The screen coordinates sent to the set function have left_x and top_y subtracted from them, moving them to be relative to the camera.
-    Notice that it calls set_active_console(0).
-     */
-    pub fn render(&self, ctx: &mut BTerm, camera: &Camera ){
-        ctx.set_active_console(0);
-        for y in camera.top_y .. camera.bottom_y {
-            for x in camera.left_x .. camera.right_x {
-                if self.in_bounds(Point::new(x,y)) {
-                    let idx = map_idx(x,y);
-                    match self.tiles[idx] {
-                        TileType::Floor => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                WHITE,
-                                BLACK,
-                                to_cp437('.')
-                            );
-                        }
-                        TileType::Wall => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                WHITE,
-                                BLACK,
-                                to_cp437('#')
-                            );
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /*
     This function checks that the location specified in point is greater
     than 0 on both the x and y axes and that it’s less than the screen height and width.
      */
